9dda1d70f1697ecd8b8aaad5a5f7c542bf43b72b
AndreiFlorentin1999 <aflorentin415@gmail.com>
2025-02-23 22:08:17 +0200

    Update endpoints.js

--- api/endpoints.js
+++ api/endpoints.js
@@ -1,6 +1,6 @@
 // API Key-urile NU ar trebui sa fie stocate in format text, nu este sigur.
 // Din pacate, fara un server, nu avem cum sa fim 100% safe, dar API-ul fiind gratuit, in cel mai rau caz cineva ne va bloca aplicatia temporar.
-const API_KEY = "fc939fd77ea2d21ce7e9262f4f6cb604";
+const API_KEY = "1dfc785b002d5d665921da5dfaba679b";
 
 function getCurrentWeatherEndpoint(city) {
   return `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ro&units=metric&appid=${API_KEY}`;
