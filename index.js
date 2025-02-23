b541d7dea70a2790ccae9d02c5809b9f0490b565
AndreiFlorentin1999 <aflorentin415@gmail.com>
2025-02-23 21:53:17 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+const currentCityTag = document.querySelector(".current-city");
+let currentCity = localStorage.getItem("city");
+
+
+if (!currentCity) {
+  localStorage.setItem("city", "București");
+  currentCity = "București";
+}
+
+
+currentCityTag.innerHTML = currentCity;
+
+
+displayCurrentWeather(currentCity);
+displayWeatherForecast(currentCity);
+
+
+const scrollToTopButton = document.querySelector(".scroll-to-top");
+
+scrollToTopButton.addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+document.addEventListener("scroll", function () {
+  if (window.scrollY > 100) {
+    scrollToTopButton.style.visibility = "visible";
+  } else {
+    scrollToTopButton.style.visibility = "hidden";
+  }
+});
+
+usernameInput.addEventListener("change", function (event) {
+  const value = event.target.value;
+  if (value.length < 3) {
+    usernameError.innerHTML =
+      "Username-ul trebuie sa contina minim 3 caractere.";
+  } else {
+    usernameError.innerHTML = "";
+  }
+});
