a0e1780fd28b6a366ff44091e9e3cc3ac44b3cc3
AndreiFlorentin1999 <aflorentin415@gmail.com>
2025-02-23 21:42:23 +0200

    Add files via upload

---
+++ endpoints.js
@@ -0,0 +1,11 @@
+// API Key-urile NU ar trebui sa fie stocate in format text, nu este sigur.
+// Din pacate, fara un server, nu avem cum sa fim 100% safe, dar API-ul fiind gratuit, in cel mai rau caz cineva ne va bloca aplicatia temporar.
+const API_KEY = "fc939fd77ea2d21ce7e9262f4f6cb604";
+
+function getCurrentWeatherEndpoint(city) {
+  return `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ro&units=metric&appid=${API_KEY}`;
+}
+
+function getForecastEndpoint(city) {
+  return `https://api.openweathermap.org/data/2.5/forecast?q=${city}&lang=ro&units=metric&appid=${API_KEY}`;
+}
